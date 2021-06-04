5ae5ed68fd63871e5c625e25efccff94a33c4ef1
wellbrez <bld-wellington@hotmail.com>
2021-06-04 15:58:37 -0300

    Update interacao_dedos.js

--- Plano Cartesiano/interacao_dedos.js
+++ Plano Cartesiano/interacao_dedos.js
@@ -71,13 +71,24 @@ $("#canvas2").on("touchstart touchmove touchend touchcancel touchleave",function
     }
     function handleEnd(evt) 
     {
-        poligono_selecionado = 0;
-
-        console.log(event);
-        polygons[poligono_selecionado].add_ponto_por_pixel(sketch.mouseX,sketch.mouseY);
-        polygons[poligono_selecionado].definir_inicio_da_animacao_de_ajuste();
-        adjtime=20;
-        atualizarUI();
+        evt.preventDefault;
+        qtd_toques = evt.originalEvent.targetTouches.length;
+        
+        if(qtd_toques==1)
+        {
+            pagexnew1 = evt.originalEvent.targetTouches[0].pageX
+            pageynew1 = evt.originalEvent.targetTouches[0].pageY
+            if(((pagexstart1-pagexnew1)**2+(pageystart1 - pageynew1)**2)**.5<=20)
+            {
+                poligono_selecionado = 0;
+                console.log(evt);
+                polygons[poligono_selecionado].add_ponto_por_pixel(sketch.mouseX,sketch.mouseY);
+                polygons[poligono_selecionado].definir_inicio_da_animacao_de_ajuste();
+                adjtime=20;
+                atualizarUI();
+            }
+        }
+        
     }
     function handleCancel(evt) 
     {
