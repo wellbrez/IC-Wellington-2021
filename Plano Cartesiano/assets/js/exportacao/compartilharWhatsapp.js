e741b8084ad63e1473dbd61a87563cfae5c9013a
wellbrez <bld-wellington@hotmail.com>
2021-08-20 14:25:54 -0300

    Whatsapp compartilhamento

--- Plano Cartesiano/assets/js/exportacao/compartilharWhatsapp.js
+++ Plano Cartesiano/assets/js/exportacao/compartilharWhatsapp.js
@@ -1,6 +1,9 @@
-function compartilharWhatsapp()
-{
-    let link = gerarCodigo(true);
-
-    
+function compartilharWhatsapp() {
+  let link = gerarCodigo(true);
+  let texto = encodeURI(
+    `Olá! Estou compartilhando uma seção transversal que fiz no app Núcleo Central. \nPara acessar, utilize o link \n${link}`
+  );
+  let walink = `https://wa.me/?text=${texto}%20`;
+  console.log("cheguei aqui");
+  window.open(walink);
 }
--- Plano Cartesiano/assets/js/importacao/decode.js
+++ Plano Cartesiano/assets/js/importacao/decode.js
@@ -1,65 +1,62 @@
-
 let filestring;
 
-function decode(dadosCompactados)
-{
-	if (dadosCompactados)
-	{
-		dadosCompactados = decodeURI(dadosCompactados);
-		const dadosEmString = dadosCompactados;
-		const dadosSeparados = dadosEmString.split(";");
+function decode(dadosCompactados) {
+  if (dadosCompactados) {
+    dadosCompactados = decodeURI(dadosCompactados);
+    const dadosEmString = dadosCompactados;
+    const dadosSeparados = dadosEmString.split(";");
 
-		let indicePoligono=0;
-		let indiceDados = 0;
-		while(indiceDados<dadosSeparados.length)
-		{
-			let [nome,qtdPontos] = [dadosSeparados[indiceDados],dadosSeparados[indiceDados+1]]
+    let indicePoligono = 0;
+    let indiceDados = 0;
+    while (indiceDados < dadosSeparados.length) {
+      let [nome, qtdPontos] = [
+        dadosSeparados[indiceDados],
+        dadosSeparados[indiceDados + 1],
+      ];
 
-			poligonos.push(new Polygon());
+      poligonos.push(new Polygon());
 
-			poligonos[indicePoligono].nome = nome;
-			selecionar(indicePoligono);
+      poligonos[indicePoligono].nome = nome;
+      selecionar(indicePoligono);
 
-			qtdPontos = Number(qtdPontos);
+      qtdPontos = Number(qtdPontos);
 
-			indiceDados+=2;
+      indiceDados += 2;
 
-			for(let ipt=0;ipt<qtdPontos;ipt++)
-			{
-				let x=Number(dadosSeparados[indiceDados])
-				let y=Number(dadosSeparados[indiceDados+1])
-				let ptTemporario = new ponto(x,y);
-				poligonos[indicePoligono].pontos.push(ptTemporario);
-				indiceDados+=2;
-			}
-			indicePoligono++;
-			atualizarUI();
-		}
-	}
-	else
-	{
-		window.alert("Selecione um arquivo válido antes de clicar em importar");
-		window.console.error("A funcao decode foi chamada para uma string indefinida.");
-	}
+      for (let ipt = 0; ipt < qtdPontos; ipt++) {
+        let x = Number(dadosSeparados[indiceDados]);
+        let y = Number(dadosSeparados[indiceDados + 1]);
+        let ptTemporario = new ponto(x, y);
+        poligonos[indicePoligono].pontos.push(ptTemporario);
+        indiceDados += 2;
+      }
+      indicePoligono++;
+      atualizarUI();
+    }
+  } else {
+    window.alert("Selecione um arquivo válido antes de clicar em importar");
+    window.console.error(
+      "A funcao decode foi chamada para uma string indefinida."
+    );
+  }
 }
 
+function carregarImportacaoInicial() {
+  const link = location.href;
+  let informacao = "";
+  if (link.search("#") != -1) {
+    informacao = link.split("#")[1];
+  } else {
+    informacao = link.split("?")[1];
+  }
+  let informacaoLocal = window.localStorage.getItem("lastsave");
 
-function carregarImportacaoInicial()
-{
-	let informacao = location.href.split("#")[1];
-	let informacaoLocal = window.localStorage.getItem('lastsave');
-
-	if (informacao)
-	{
-		decode(informacao);
-	}
-	else if (informacaoLocal)
-	{
-		decode(informacaoLocal);
-	}
-	else
-	{
-		poligonos.push(new Polygon());
-	}
-	atualizarUI();
+  if (informacao) {
+    decode(informacao);
+  } else if (informacaoLocal) {
+    decode(informacaoLocal);
+  } else {
+    poligonos.push(new Polygon());
+  }
+  atualizarUI();
 }
