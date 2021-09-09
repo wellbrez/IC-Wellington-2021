c6a62412bbfe68a0ffc473edae2bf03e097242d9
wellbrez <bld-wellington@hotmail.com>
2021-09-09 16:24:49 -0300

    final-commit

--- Nucleo Central/assets/js/desenhos/canvas.js
+++ Nucleo Central/assets/js/desenhos/canvas.js
@@ -1,110 +1,89 @@
-
-
-function resizewindow()
-{
-	width = document.body.getBoundingClientRect().width;
-	//let width = screen.width;
-	height = document.body.getBoundingClientRect().height;
-	//let height = screen.height;
-	window.sketch.resizeCanvas(width,height,true);
+function resizewindow() {
+  width = document.body.getBoundingClientRect().width;
+  //let width = screen.width;
+  height = document.body.getBoundingClientRect().height;
+  //let height = screen.height;
+  window.sketch.resizeCanvas(width, height, true);
 }
-var sketch = function(p)
-{
-	p.setup = function()
-	{
-		d_circle = 0;
-		toquex=0;
-		toquey=0;
-		p.frameRate(60)
-		eventoZoom = null;
-		eventoScroll = null;
-		//width = (p.windowWidth);
-		//height = (p.windowHeight);
-		width = document.body.getBoundingClientRect().width;
-		height = document.body.getBoundingClientRect().height;
-		p.cnv = p.createCanvas(width,height);
+var sketch = function (p) {
+  p.setup = function () {
+    d_circle = 0;
+    toquex = 0;
+    toquey = 0;
+    p.frameRate(60);
+    eventoZoom = null;
+    eventoScroll = null;
+    //width = (p.windowWidth);
+    //height = (p.windowHeight);
+    width = document.body.getBoundingClientRect().width;
+    height = document.body.getBoundingClientRect().height;
+    p.cnv = p.createCanvas(width, height);
 
-		escalax=1;
-		escalay=1;
-		iglobal = 0;
+    escalax = 1;
+    escalay = 1;
+    iglobal = 0;
 
-		origemX = 0;
-		origemY = 0;
+    origemX = 0;
+    origemY = 0;
 
-		origin_pixeldistx = origemX*escalax;
-		origin_pixeldisty = origemY*escalay;
+    origin_pixeldistx = origemX * escalax;
+    origin_pixeldisty = origemY * escalay;
 
-		intervalo = 10;
-		carregarImportacaoInicial();
-		let grahamScan;
-		poligonos[poligonos.length-1].selected = true;
+    intervalo = 10;
+    carregarImportacaoInicial();
+    let grahamScan;
+    poligonos[poligonos.length - 1].selected = true;
 
-		
-		
-		mostrar_area = false;
+    mostrar_area = false;
 
-		
-		resizewindow();
-
-	}/*
+    resizewindow();
+  }; /*
 	p.mouseWheel = function(event)
 	{
 		mouseWheel(event);
 	}*/
-	p.draw = function()
-	{
-		p.clear();
-		resizewindow();
-		//distância, em pixels, da origem do plano cartesiano (atualizacao)
-		origin_pixeldistx = origemX*escalax;
-		origin_pixeldisty = origemY*escalay;
-		//definindo cor de fundo do canvas
-		p.background(corFundo);
-		//translacção ao meio da tela
-		p.translate(width/2,height/2);
-		//definicao do intervalo entre eixos secundarios
-		att_propriedades_canvas()
-	
-		
-		grade(p,intervalo,'principal');
-		grade(p,intervalo/5,'secundaria');
-		linhas_principais(p,corLinhasPrincipais);
-		desenharPoligonos(poligonos,p,corPoligono)
-		calcularEnvoltoria(poligonos,propriedadesGlobais)
-		desenharEnvoltoria(propriedadesGlobais);
-		desenharNC(propriedadesGlobais,p)
-		eixosPrincipais(propriedadesGlobais);
-		
-		
-		if(mostrarLN)
-		{
-			let [pols, propriedadesAreaComprimida] = LN(p);
-			let temPoligonoInvalido = false;
-			calcularPropriedadesConjunto(pols,propriedadesAreaComprimida)
-			for(let pol of pols)
-			{
-				if(!pol.isValid)
-				{
-					temPoligonoInvalido = true;
-				}
-			}
-			if(!temPoligonoInvalido)
-			{
-				desenharNC(propriedadesAreaComprimida)
-			}
-		}
+  p.draw = function () {
+    p.clear();
+    resizewindow();
+    //distância, em pixels, da origem do plano cartesiano (atualizacao)
+    origin_pixeldistx = origemX * escalax;
+    origin_pixeldisty = origemY * escalay;
+    //definindo cor de fundo do canvas
+    p.background(corFundo);
+    //translacção ao meio da tela
+    p.translate(width / 2, height / 2);
+    //definicao do intervalo entre eixos secundarios
+    att_propriedades_canvas();
 
-		if(eventoScroll!=null){
-			eventoScroll.aplicar()};
-		if(eventoZoom!=null){
-		eventoZoom.aplicar()} 
-	}
-}
+    grade(p, intervalo, "principal");
+    grade(p, intervalo / 5, "secundaria");
+    linhas_principais(p, corLinhasPrincipais);
+    desenharPoligonos(poligonos, p, corPoligono);
+    calcularEnvoltoria(poligonos, propriedadesGlobais);
+    desenharEnvoltoria(propriedadesGlobais);
+    console.log(propriedadesGlobais);
+    desenharNC(propriedadesGlobais, p);
+    eixosPrincipais(propriedadesGlobais);
 
+    if (mostrarLN) {
+      let [pols, propriedadesAreaComprimida] = LN(p);
+      let temPoligonoInvalido = false;
+      calcularPropriedadesConjunto(pols, propriedadesAreaComprimida);
+      for (let pol of pols) {
+        if (!pol.isValid) {
+          temPoligonoInvalido = true;
+        }
+      }
+      if (!temPoligonoInvalido) {
+        desenharNC(propriedadesAreaComprimida);
+      }
+    }
 
-
-
-
-
-
-
+    if (eventoScroll != null) {
+      eventoScroll.aplicar();
+    }
+    if (eventoZoom != null) {
+      eventoZoom.aplicar();
+    }
+  };
+};
--- Nucleo Central/assets/js/elementosNucleoCentral/envoltoria.js
+++ Nucleo Central/assets/js/elementosNucleoCentral/envoltoria.js
@@ -1,43 +1,37 @@
 //iniciando método
-function calcularEnvoltoria(poligonos,conjunto)
-{
-    let grahamScan = new GrahamScan();
-    pontosTotais = [];
-    if(!poligonos) return;
-    for (let poligono of poligonos)
-    {
-        if(poligono.positivo)
-        {
-            for(let i=0;i<poligono.pontos.length;i++)
-            {
-                pontosTotais.push([poligono.pontos[i].x,poligono.pontos[i].y]);
-            }
-        }
+function calcularEnvoltoria(poligonos, conjunto) {
+  let grahamScan = new GrahamScan();
+  pontosTotais = [];
+  if (!poligonos) return;
+  for (let poligono of poligonos) {
+    if (poligono.positivo) {
+      for (let i = 0; i < poligono.pontos.length; i++) {
+        pontosTotais.push([poligono.pontos[i].x, poligono.pontos[i].y]);
+      }
     }
-    
-    grahamScan.setPoints(pontosTotais);
-    envoltoriaCrua = grahamScan.getHull();  // [1,0], [2,1], [0,1]
-    conjunto.pontosDaEnvoltoria = [];
-    for (let point of envoltoriaCrua)
-    {
-        conjunto.pontosDaEnvoltoria.push(point[0]);
-        conjunto.pontosDaEnvoltoria.push(point[1]);
-    }
-    return conjunto.pontosDaEnvoltoria;
-}
-function desenharEnvoltoria(conjunto)
-{
-    let pontos = conjunto.envoltoria;
-    if(!pontos) return
-    sketch.stroke(corEnvoltoria);
-	sketch.strokeWeight(3.9);
-	sketch.beginShape();
-    sketch.fill(0,0,0,0);
-    if(!pontos) return;
-	for(let i=0;i<pontos.length;i+=2)
-	{
-		sketch.vertex(pixelX(pontos[i]),pixelY(pontos[i+1]));
-	}
+  }
 
-	sketch.endShape(sketch.CLOSE)
+  grahamScan.setPoints(pontosTotais);
+  envoltoriaCrua = grahamScan.getHull(); // [1,0], [2,1], [0,1]
+  conjunto.pontosDaEnvoltoria = [];
+  for (let point of envoltoriaCrua) {
+    conjunto.pontosDaEnvoltoria.push(point[0]);
+    conjunto.pontosDaEnvoltoria.push(point[1]);
+  }
+  return conjunto.pontosDaEnvoltoria;
+}
+function desenharEnvoltoria(conjunto) {
+  let pontos = conjunto.pontosDaEnvoltoria;
+  if (!pontos) return;
+  console.log("rodei");
+  sketch.stroke(corEnvoltoria);
+  sketch.strokeWeight(3.9);
+  sketch.beginShape();
+  sketch.fill(0, 0, 0, 0);
+  if (!pontos) return;
+  for (let i = 0; i < pontos.length; i += 2) {
+    sketch.vertex(pixelX(pontos[i]), pixelY(pontos[i + 1]));
+  }
+
+  sketch.endShape(sketch.CLOSE);
 }
