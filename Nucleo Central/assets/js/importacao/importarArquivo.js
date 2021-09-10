d19185b3871e0c2fadcb7048bdc7d87a20b5c8b1
wellbrez <bld-wellington@hotmail.com>
2021-09-10 16:19:19 -0300

    bugfix

--- Nucleo Central/assets/js/importacao/importarArquivo.js
+++ Nucleo Central/assets/js/importacao/importarArquivo.js
@@ -1,19 +1,16 @@
-function ativarInputDeArquivo()
-{
-	document.getElementById('inputfile').addEventListener('change', function() 
-	{
-		let fr= new FileReader(); 
-		fr.onload=function()
-		{ 
-			filestring = fr.result;
-		} 
-		fr.readAsText(this.files[0]); 
-	}) 
+function ativarInputDeArquivo() {
+  document.getElementById("inputfile").addEventListener("change", function () {
+    let fr = new FileReader();
+    fr.onload = function () {
+      filestring = fr.result;
+    };
+    fr.readAsText(this.files[0]);
+  });
 }
-function importar_arquivo()
-{
-    poligonos = [];
-    decode(filestring);
+function importar_arquivo() {
+  poligonos = [];
+  filestring = filestring.replace(/[\r\n]*/g, "");
+  decode(filestring);
 }
 
 ativarInputDeArquivo();
