adcbbf81e3539a4f8e2132d0c48d20995b6be4f2
wellbrez <bld-wellington@hotmail.com>
2021-09-12 15:19:57 -0300

    BugFix

--- Plano Cartesiano/assets/js/interacao/funcoesDedos.js
+++
@@ -1,9 +0,0 @@
-function dois_toques_pinca(dzoom, meio_dos_dedos_x, meio_dos_dedos_y) {
-  eventoZoom = new zoom(
-    1,
-    dzoom / escalax,
-    dzoom / escalay,
-    meio_dos_dedos_x,
-    meio_dos_dedos_y
-  );
-}
--- Plano Cartesiano/assets/js/interacao/interacaoDedos.js
+++ Plano Cartesiano/assets/js/interacao/interacaoDedos.js
@@ -167,3 +167,13 @@ window.addEventListener("keydown", function (event) {
       return; // Quit when this doesn't handle the key event.
   }
 });
+
+function dois_toques_pinca(dzoom, meio_dos_dedos_x, meio_dos_dedos_y) {
+  eventoZoom = new zoom(
+    1,
+    dzoom / escalax,
+    dzoom / escalay,
+    meio_dos_dedos_x,
+    meio_dos_dedos_y
+  );
+}
